0de66dfce5a47d8138897c65b1eb87b0ffa89a11
Ramadevi09 <rdevi2850@gmail.com>
2019-07-11 23:09:40 +0530

    Spring aware interfaces

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -7,13 +7,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 public static void main(String[]args) {
     //using ApplicationContext
+
     ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+    System.out.println("============================================");
+    System.out.println("in main class applicationcontext");
     Moive movie = context.getBean("movie", Moive.class);
     movie.moiveDisplay();
-    Moive moive1=context.getBean("movie1",Moive.class);
-    moive1.moiveDisplay();
-    Moive moive2=context.getBean("movie2",Moive.class);
-    moive2.moiveDisplay();
+
 
 
 
--- src/main/java/com/stackroute/domain/Moive.java
+++ src/main/java/com/stackroute/domain/Moive.java
@@ -1,9 +1,17 @@
 package com.stackroute.domain;
 
-public class Moive {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Moive implements ApplicationContextAware, BeanNameAware, BeanFactoryAware {
     //actor class instantiation
     private Actor actor;
-   public Moive(){
+    private ApplicationContext applicationContext;
+    public Moive(){
     }
 
     public Moive(Actor actor) {
@@ -21,4 +29,21 @@ public void moiveDisplay(){
 }
 
 
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+this.applicationContext=applicationContext;
+        System.out.println("implemented applicationContext interface in movie");
+        Moive moive=applicationContext.getBean("movie",Moive.class);
+        moive.moiveDisplay();
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("bean name is :"+ s);
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("bean factory is :"+beanFactory);
+    }
 }
